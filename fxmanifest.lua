f0f1bb9a82cf6c584f3f0febe2ae208d37016e3b
Stausi <jonasbs@live.dk>
2025-01-21 00:00:22 +0100

    Another try for release

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,11 +9,11 @@ lua54 'yes'
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    "server.lua",
+    "server/main.lua",
 }
 
 client_scripts {
-    "client.lua"
+    "client/main.lua"
 }
 
 shared_scripts {
